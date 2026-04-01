a84baba3f38231b5f35f894ed4b9dd03a0707e80
phantekzy <mainilotfi@gmail.com>
2026-04-01 20:02:20 +0100

    Pattern Matching

--- src/error.rs
+++ src/error.rs
@@ -12,5 +12,13 @@ pub enum VivecError {
 impl std::error::Error for VivecError {}
 
 impl fmt::Display for VivecError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            VivecError::Io(e) => write!(f, "IO Error : {}", e),
+            VivecError::Parse(s) => write!(f, "Parsing Error : {}", s),
+            VivecError::Network(s) => writeln!(f, "Network Error : {}", s),
+            VivecError::InvalidUrl(u) => writeln!(f, "Invalid URL : {}", u),
+            VivecError::Timeout => writeln!(f, "Request timed out"),
+        }
+    }
 }
