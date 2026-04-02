970ec861542c4c87776cac23affb7591b01a426e
phantekzy <mainilotfi@gmail.com>
2026-04-02 12:55:58 +0100

    added an enum and struct

--- src/request.rs
+++ src/request.rs
@@ -1,6 +1,15 @@
+use std::collections::HashMap;
+
 pub enum Method {
     GET,
     POST,
     PUT,
     DELETE,
+}
+
+struct Request {
+    pub method: Method,
+    pub url: String,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
 }
