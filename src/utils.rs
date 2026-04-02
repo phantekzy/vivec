2a84a475be990aeab6396e0711e9a1f8c37f90a4
phantekzy <mainilotfi@gmail.com>
2026-04-02 09:58:41 +0100

    split the IP address/domaine from the port number

--- src/utils.rs
+++ src/utils.rs
@@ -5,6 +5,13 @@ pub struct ParsedUrl {
 }
 
 pub fn parse_url(url: &str) -> Result<ParsedUrl, String> {
-    let remainder = url.strip_prefix("http://").ok_or("Only Http supported")?;
-    let (host_port, path_query) = remainder.split_once("/").unwrap_or((remainder, ""));
+    let remainder = url
+        .strip_prefix("http://")
+        .ok_or("Only Http is supported")?;
+    let (host_port, path_query) = remainder.split_once('/').unwrap_or((remainder, ""));
+    let (host, port) = if let Some((h, p)) = host_port.split_once(':') {
+        (h.to_string(), p.to_string())
+    } else {
+        (host_port.to_string(), "80".to_string())
+    };
 }
