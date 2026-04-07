57c08bab08f0bff478f9c054873cacb0e439fde2
phantekzy <mainilotfi@gmail.com>
2026-04-07 11:38:19 +0100

    parsing the response

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod error;
+pub mod parser;
 pub mod request;
 pub mod response;
 pub mod utils;
---
+++ src/parser.rs
@@ -0,0 +1,3 @@
+use crate::{error::XsusError, response::Response};
+
+pub fn parse_response(raw: &str) -> Result<Response, XsusError> {}
