428838799a0e8548769d12680345b43d453529ff
phantekzy <mainilotfi@gmail.com>
2026-04-07 14:52:15 +0100

    status lines

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,8 @@
 use crate::{error::XsusError, response::Response};
 
-pub fn parse_response(raw: &str) -> Result<Response, XsusError> {}
+pub fn parse_response(raw: &str) -> Result<Response, XsusError> {
+    let mut lines = raw.lines();
+    let status_lines = lines
+        .next()
+        .ok_or(XsusError::Parse("Empty response".into()))?;
+}
