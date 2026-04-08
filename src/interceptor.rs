6f78eb8feab71d2e32d93eb4ad9210378ff37816
phantekzy <mainilotfi@gmail.com>
2026-04-08 09:54:15 +0100

    added Interceptors struct

--- src/interceptor.rs
+++ src/interceptor.rs
@@ -2,3 +2,8 @@ use crate::{request::Request, response::Response};
 
 pub type RequestInterceptor = Box<dyn Fn(Request) -> Request>;
 pub type ResponseInterceptor = Box<dyn Fn(Response) -> Response>;
+
+pub struct Interceptors {
+    pub request: Vec<RequestInterceptor>,
+    pub response: Vec<ResponseInterceptor>,
+}
