e8029127ffe692f47ce6ef985a2f3a09ea9549d3
phantekzy <mainilotfi@gmail.com>
2026-04-12 15:51:40 +0100

    fixed the fucking dns error , i used the ToSocketAddrs to resolve hostnames and manages the raw TCP stream

--- src/transport.rs
+++ src/transport.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{Read, Write},
-    net::TcpStream,
+    net::{TcpStream, ToSocketAddrs},
     time::Duration,
 };
 
@@ -9,17 +9,27 @@ use crate::{error::XsusError, request::Request, utils::parse_url};
 pub fn execute_network_call(req: &Request, timeout: Duration) -> Result<String, XsusError> {
     let url_info = parse_url(&req.url).map_err(XsusError::InvalidUrl)?;
     let addr = format!("{}:{}", url_info.host, url_info.port);
-    let mut stream = TcpStream::connect_timeout(
-        &addr
-            .parse()
-            .map_err(|_| XsusError::Network("Could not resolve host".into()))?,
-        timeout,
-    )?;
+
+    let socket_addrs = addr
+        .to_socket_addrs()
+        .map_err(|_| XsusError::Network(format!("Could not resolve host: {}", url_info.host)))?;
+
+    let mut stream = None;
+    for sa in socket_addrs {
+        if let Ok(s) = TcpStream::connect_timeout(&sa, timeout) {
+            stream = Some(s);
+            break;
+        }
+    }
+
+    let mut stream = stream.ok_or(XsusError::Network("Failed to connect to host".into()))?;
+
     stream.set_read_timeout(Some(timeout))?;
     stream.set_write_timeout(Some(timeout))?;
 
     let raw_http = req.to_http_string(&url_info.path, &url_info.host);
     stream.write_all(raw_http.as_bytes())?;
+    stream.flush()?;
 
     let mut response_text = String::new();
     stream.read_to_string(&mut response_text)?;
